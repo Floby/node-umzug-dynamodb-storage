2ad8da250c7ed0775c26ac46da3d63af89b13261
Florent Jaby <florent.jaby@gmail.com>
2018-03-05 16:14:34 +0100

    Implement dynamoDB storage for umzug

---
+++ index.js
@@ -0,0 +1,70 @@
+const Joi = require('joi');
+const delay = require('delay');
+
+module.exports = DynamoDbStorage;
+
+function DynamoDbStorage({ dynamodb, table = 'umzug-migrations' }) {
+  const dynogels = require('dynogels-promisified');
+  dynogels.dynamoDriver(dynamodb);
+
+  const Migrations = dynogels.define(table, {
+    hashKey: 'name',
+    timestamps: true,
+    schema: {
+      name: Joi.string().required(),
+      executed: Joi.boolean().default(false)
+    }
+  });
+  delete dynogels.models[table]; // otherwise dynogels.createAll/deleteAll messes up
+
+  this.logMigration = (migrationName) => {
+    return ensureMigrationTable().then(() => {
+      return Migrations.createAsync({
+        name: migrationName,
+        executed: true
+      });
+    });
+  };
+
+  this.unlogMigration = (migrationName) => {
+    return ensureMigrationTable().then(() => {
+      return Migrations.destroyAsync(migrationName);
+    });
+  };
+
+  this.executed = () => {
+    return ensureMigrationTable()
+      .then(() => {
+        return Migrations.scan()
+          .where('executed')
+          .equals(true)
+          .loadAll()
+          .execAsync();
+      })
+      .then(({ Items }) => {
+        return Items.map((item) => item.get('name')).sort();
+      });
+  };
+
+  function ensureMigrationTable() {
+    return Migrations.describeTableAsync()
+      .then((description) => {
+        return description
+          ? Migrations.updateTableAsync
+          : Migrations.createTableAsync();
+      })
+      .catch(() => {
+        return Migrations.createTableAsync();
+      })
+      .then(() => {
+        return checkActive();
+        function checkActive() {
+          return Migrations.describeTableAsync().then(({ Table }) => {
+            if (Table.TableStatus !== 'ACTIVE') {
+              return delay(500).then(checkActive);
+            }
+          });
+        }
+      });
+  }
+}
---
+++ test/dynamodb-storage.test.js
@@ -0,0 +1,112 @@
+const dynalite = require('dynalite');
+const DynamoDbStorage = require('../');
+const AWS = require('aws-sdk');
+const pify = require('pify');
+const { expect } = require('chai')
+const Path = require('path');
+
+describe('new DynamoDbStorage({ dynamodb, table })', function () {
+  //this.timeout(20000)
+  const migrationName = '0125678-some-migration.js';
+  const tableName = 'some-migrations';
+  let dynamoDbStorage;
+
+  const dynamodb = withDynalite(1000);
+
+  beforeEach(() => {
+    dynamoDbStorage = new DynamoDbStorage({ dynamodb, table: tableName });
+  });
+
+  it('returns an instance of DynamoDbStorage', () => {
+    expect(dynamoDbStorage).to.be.an.instanceOf(DynamoDbStorage);
+  });
+
+  describe('.logMigration(name)', () => {
+    it('inserts a row for this migration', () => {
+      // Given
+      const getItem = pify(dynamodb.getItem.bind(dynamodb));
+      // When
+      return dynamoDbStorage.logMigration(migrationName).then(() => {
+        // Then
+        return getItem({
+          TableName: tableName,
+          Key: {
+            name: { S: migrationName }
+          }
+        }).then(({ Item }) => {
+          expect(Item.name.S).to.equal(migrationName);
+          expect(Item.executed.BOOL).to.equal(true);
+        });
+      });
+    });
+  });
+
+  describe('.unlogMigration(name)', () => {
+    beforeEach(() => {
+      return dynamoDbStorage.logMigration(migrationName);
+    });
+
+    it('remove the row for this migration', () => {
+      // Given
+      const getItem = pify(dynamodb.getItem.bind(dynamodb));
+      // When
+      return dynamoDbStorage.unlogMigration(migrationName).then(() => {
+        // Then
+        return getItem({
+          TableName: tableName,
+          Key: {
+            name: { S: migrationName }
+          }
+        }).then(({ Item }) => {
+          expect(Item).to.equal(undefined);
+        });
+      });
+    });
+  });
+
+  describe('.executed()', () => {
+    describe('when there were no migrations', () => {
+      it('resolves []', () => {
+        // When
+        return dynamoDbStorage.executed().then((actual) => {
+          // Then
+          expect(actual).to.deep.equal([]);
+        });
+      });
+    });
+
+    describe('when there were successful migrations', () => {
+      beforeEach(() => dynamoDbStorage.logMigration('1234-hello.js'));
+      beforeEach(() => dynamoDbStorage.logMigration('5678-goodbye.js'));
+      it('lists all passed migrations', () => {
+        // When
+        return dynamoDbStorage.executed().then((actual) => {
+          // Then
+          expect(actual).to.deep.equal(['1234-hello.js', '5678-goodbye.js']);
+        });
+      });
+    });
+  });
+});
+
+function withDynalite(createTableMs = 10) {
+  const portNumber = 5252
+  const dynaliteServer = dynalite({
+    createTableMs
+  });
+
+  beforeEach((done) => {
+    dynaliteServer.listen(portNumber, done)
+  });
+
+  afterEach((done) => {
+    dynaliteServer.close(done)
+  });
+
+  const dynamodb = new AWS.DynamoDB({
+    endpoint: 'http://localhost:' + portNumber,
+    region: 'eu-west-1'
+  });
+
+  return dynamodb;
+}
